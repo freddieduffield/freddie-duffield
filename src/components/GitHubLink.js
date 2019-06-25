b3e544b5aa8c99f6a080b1ac1b74458067c4cc06
FrederickFoxxx <fwhduffield@gmail.com>
2019-06-25 07:41:27 +0100

    add git hub link

---
+++ src/components/GitHubLink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GitHub from '../ui/GitHub';
+
+const GitHubLink = () => {
+  return (
+    <a href="https://github.com/frederickfoxxx">
+      <GitHub />
+    </a>
+  );
+};
+
+export default GitHubLink;
---
+++ src/ui/GitHub.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Github } from 'styled-icons/feather/Github';
+
+const GitHubI = styled(Github)`
+  color: black;
+  cursor: pointer;
+  height: 100px;
+  position: relative;
+  z-index: 3;
+`;
+
+export default GitHubI;
